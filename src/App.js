8310f0a2c04e03a79e226d286f580cf1c0a6281a
Yaak0v <jacobsandry@gmail.com>
2023-04-08 13:31:55 -0400

    Not a great stopping point, but add some text fields to the main form and begin working on the select field.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import { makeStyles } from "@material-ui/core/styles";
 import { Container, Grid, Typography } from "@material-ui/core";
+import Textfield from './Components/FormsUI/Textfield'
 
 const useStyles = makeStyles((theme) => ({
   formWrapper: {
@@ -12,13 +13,34 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const INITIAL_FORM_STATE = {};
+const INITIAL_FORM_STATE = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  phone: "",
+  addressline1: '',
+  addressline2: '',
+  city: '',
+  state: '',
+  country: '',
+};
 
-const FORM_VALIDATION = Yup.object().shape({});
+const FORM_VALIDATION = Yup.object().shape({
+  firstName: Yup.string().required("Required"),
+  lastName: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid email.").required("Required"),
+  addressLine1: Yup.string().required('Required'),
+  addressLine2: Yup.string(),
+  city: Yup.string().required('Required'),
+  state: Yup.string().required('Required')
+
+  // phone: Yup.number()
+  //   .integer
+  //   .required("Required"),
+});
 
 const App = () => {
   const classes = useStyles();
-
   return (
     <Grid container>
       <Header />
@@ -40,8 +62,33 @@ const App = () => {
                     <Typography>Your details</Typography>
                   </Grid>
 
+                  <Grid item xs={6}>
+                    <Textfield name="firstName" label="First Name" />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Textfield name="lastName" label="Last Name" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Textfield name="email" label="Email" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Textfield name="phone" label="Phone Number" />
+                  </Grid>
+
                   <Grid item xs={12}>
                     <Typography>Address</Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Textfield name="addressLine1" label="Address Line 1" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Textfield name="addressLine2" label="Address Line 2" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Textfield name="city" label="City" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Textfield name="state" label="State" />
                   </Grid>
 
                   <Grid item xs={12}>
--- src/Components/FormsUI/Select/index.js
+++ src/Components/FormsUI/Select/index.js
@@ -1,45 +1,25 @@
-import React from 'react';
-import { TextField, MenuItem } from '@material-ui/core';
-import { useField, useFormikContext } from 'formik';
+import React from "react";
+import { TextField, MenuItem } from "@material-ui/core";
+import { useField, useFormik, useFormikContext } from "formik";
 
-const SelectWrapper = ({
-  name,
-  options,
-  ...otherProps
-}) => {
+const SelectWrapper = ({ name, options, ...otherProps }) => {
   const { setFieldValue } = useFormikContext();
   const [field, meta] = useField(name);
 
-  const handleChange = evt => {
-    const { value } = evt.target;
+  const handleChange = (event) => {
+    const { value } = event.target;
     setFieldValue(name, value);
   };
 
   const configSelect = {
     ...field,
-    ...otherProps,
     select: true,
-    variant: 'outlined',
+    variant: "outlined",
     fullWidth: true,
-    onChange: handleChange
+    onChange: handleChange,
   };
 
-  if (meta && meta.touched && meta.error) {
-    configSelect.error = true;
-    configSelect.helperText = meta.error;
-  }
-
-  return (
-    <TextField {...configSelect}>
-      {Object.keys(options).map((item, pos) => {
-        return (
-          <MenuItem key={pos} value={item}>
-            {options[item]}
-          </MenuItem>
-        )
-      })}
-    </TextField>
-  );
+  return <TextField {...configSelect}></TextField>;
 };
 
 export default SelectWrapper;
