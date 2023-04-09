54051f2f433c8b51368ff5d0b43dd0c26c1e09a3
Yaak0v <jacobsandry@gmail.com>
2023-04-09 16:05:42 -0400

    Add countries field to form

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import { makeStyles } from "@material-ui/core/styles";
 import { Container, Grid, Typography } from "@material-ui/core";
-import Textfield from './Components/FormsUI/Textfield'
+import Textfield from "./Components/FormsUI/Textfield";
+import Select from "./Components/FormsUI/Select";
+import countries from "./data/countries.json";
 
 const useStyles = makeStyles((theme) => ({
   formWrapper: {
@@ -18,21 +20,21 @@ const INITIAL_FORM_STATE = {
   lastName: "",
   email: "",
   phone: "",
-  addressline1: '',
-  addressline2: '',
-  city: '',
-  state: '',
-  country: '',
+  addressline1: "",
+  addressline2: "",
+  city: "",
+  state: "",
+  country: "",
 };
 
 const FORM_VALIDATION = Yup.object().shape({
   firstName: Yup.string().required("Required"),
   lastName: Yup.string().required("Required"),
   email: Yup.string().email("Invalid email.").required("Required"),
-  addressLine1: Yup.string().required('Required'),
+  addressLine1: Yup.string().required("Required"),
   addressLine2: Yup.string(),
-  city: Yup.string().required('Required'),
-  state: Yup.string().required('Required')
+  city: Yup.string().required("Required"),
+  state: Yup.string().required("Required"),
 
   // phone: Yup.number()
   //   .integer
@@ -89,6 +91,13 @@ const App = () => {
                   </Grid>
                   <Grid item xs={12}>
                     <Textfield name="state" label="State" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Select
+                      name="country"
+                      label="Country"
+                      options={countries}
+                    />
                   </Grid>
 
                   <Grid item xs={12}>
